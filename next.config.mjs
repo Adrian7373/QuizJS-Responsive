4428e7c73acb44cb50d86ef84fe685e4848c1709
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-03-05 13:15:16 +0800

    Update next.config.mjs to configure output for GitHub Pages and set basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // Required for GitHub Pages
+  basePath: '/QuizJS-Responsive', // IMPORTANT: Replace with your exact GitHub repository name
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
