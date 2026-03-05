02865fb8ebc4f7d275368c5337df06992f0e8b0c
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-03-05 14:09:54 +0800

    Update StartButton to use basePath for audio file path in production

--- src/Components/StartButton/StartButton.jsx
+++ src/Components/StartButton/StartButton.jsx
@@ -1,11 +1,13 @@
 import style from "./StartButton.module.css";
 
+const basePath = process.env.NODE_ENV === "production" ? "/quizjs-responsive" : "";
+
 export default function StartButton({ start, playAudio }) {
 
     return (
         <button className={style.startButton} onClick={() => {
             start();
-            const startAudio = new Audio("/sounds/start.mp3");
+            const startAudio = new Audio(`${basePath}/sounds/start.mp3`);
             playAudio(startAudio);
         }}>
             Start
